b5e05c1dba729800a4839c36ef3b0d884e969f33
yanqirenshi <yanqirenshi@gmail.com>
2018-08-05 20:25:00 +0900

    update dist/

--- dist/data.js
+++
@@ -1,9 +0,0 @@
-var data = [30, 86, 168, 281, 303, 365];
-
-var graph_data = [
-    {code: '1', name: 'a', start: new Date('2017-10-01'), end: new Date('2017-10-12'), detail: []},
-    {code: '2', name: 'b', start: new Date('2017-10-01'), end: new Date('2017-10-23'), detail: []},
-    {code: '3', name: 'c', start: new Date('2017-10-03'), end: new Date('2017-11-04'), detail: []},
-    {code: '4', name: 'd', start: new Date('2017-10-04'), end: new Date('2017-11-17'), detail: []},
-    {code: '5', name: 'e', start: new Date('2017-10-05'), end: new Date('2017-11-30'), detail: []},
-];
