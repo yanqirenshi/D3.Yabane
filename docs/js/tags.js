9e62a4158fba1be5fcb2bc0fb67770c275875ea4
satoshi.iwasaki <satoshi.iwasaki@glpgs.com>
2019-03-22 09:09:45 +0900

    compiled

--- docs/js/tags.js
+++ docs/js/tags.js
@@ -189,7 +189,7 @@ riot.tag2('home', '', '', '', function(opts) {
      this.on('update', () => { this.draw(); });
 });
 
-riot.tag2('home_page_root', '<section-header title="HOME"></section-header> <section class="section"> <div class="container"> <h1 class="title">CDN</h1> <h2 class="subtitle"></h2> <div class="contents"> <p>https://yanqirenshi.github.io/D3.Yabane/dist/0.0.1/d3.yabane.js</p> </div> </div> </section>', '', '', function(opts) {
+riot.tag2('home_page_root', '<section-header title="HOME"></section-header> <section class="section"> <div class="container"> <h1 class="title">CDN</h1> <h2 class="subtitle"></h2> <div class="contents"> <p>https://yanqirenshi.github.io/D3.Yabane/dist/beta/d3.yabane.js</p> <p>https://yanqirenshi.github.io/D3.Yabane/dist/0.0.1/d3.yabane.js</p> </div> </div> </section>', '', '', function(opts) {
 });
 
 riot.tag2('page01', '', '', '', function(opts) {
